e2e13069a35afebeb2b586400dd64464d918c2de
tosanche <tominegrette@hotmail.com>
2026-05-20 01:43:11 -0300

    support multiple concurrent rooms :)

--- chatroom/src/client.c
+++ chatroom/src/client.c
@@ -1,21 +1,35 @@
+#include <commons/collections/list.h>
 #include <commons/net/connection.h>
 #include <commons/net/packet.h>
+#include <commons/net/status.h>
 #include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-#define PORT "8081"
+#define PORT "8080"
 
 typedef enum {
   LOGIN,
-  CONNECTED,
-  DISCONNECTED,
   USER_ALREADY_TAKEN,
-  MESSAGE_PACKET,
+
+  JOINED,
+  LEFT,
+
+  ROOM_LIST,
+  JOIN_ROOM,
+  LEAVE_ROOM,
+
+  MESSAGE,
 } Packet_type;
 
 int client = -1;
+bool quit = false;
 char name[64] = {0};
+u_int32_t rooms = 0;
 
 int read_line(char *buffer, size_t size) {
   if (fgets(buffer, size, stdin) == NULL)
@@ -24,12 +38,12 @@ int read_line(char *buffer, size_t size) {
   return 1;
 }
 
-void handle_disconnected(t_packet *packet) {
+void handle_left(t_packet *packet) {
   const char *other = packet_read_string(packet);
   printf("%s left the room\n", other);
 }
 
-void handle_connected(t_packet *packet) {
+void handle_joined(t_packet *packet) {
   const char *other = packet_read_string(packet);
   printf("%s joined the room\n", other);
 }
@@ -41,10 +55,110 @@ void handle_message(t_packet *packet) {
 }
 
 void send_message(char *message) {
-  t_packet *packet = packet_create(MESSAGE_PACKET);
+  t_packet *packet = packet_create(MESSAGE);
   packet_add_string(packet, message);
   packet_send(packet, client);
   packet_destroy(packet);
+}
+
+void quit_program() {
+  quit = 1;
+  connection_close(client);
+}
+
+bool login() {
+  if (client != -1)
+    connection_close(client);
+
+  printf("Enter name to login: ");
+  read_line(name, sizeof(name));
+
+  client = connection_create_client("localhost", PORT);
+
+  t_packet *packet = packet_create(LOGIN);
+  packet_add_string(packet, name);
+  packet_send(packet, client);
+  packet_destroy(packet);
+
+  t_packet *res = packet_recieve(client);
+  if (status_unpack(res) == OK) {
+    packet_destroy(res);
+    return true;
+  }
+
+  if (packet->type == USER_ALREADY_TAKEN) {
+    printf("Username already taken\n");
+  }
+
+  packet_destroy(res);
+  return false;
+}
+
+u_int32_t get_rooms(t_packet *rooms_packet) {
+  u_int32_t length = packet_read_uint32(rooms_packet);
+  return length;
+}
+
+t_list *join_room(u_int32_t room) {
+  t_packet *packet = packet_create(JOIN_ROOM);
+  packet_add_uint32(packet, room);
+  packet_send(packet, client);
+  packet_destroy(packet);
+  t_packet *res = packet_recieve(client);
+
+  if (status_unpack(res) != OK) {
+    // TODO: do something, prompt again ?
+  }
+
+  t_list *people_in_room = list_create();
+  u_int32_t count = packet_read_uint32(res);
+  for (u_int32_t i = 0; i < count; i++) {
+    list_add(people_in_room, packet_read_string(res));
+  }
+  return people_in_room;
+}
+
+void leave_room() {
+  t_packet *packet = packet_create(LEAVE_ROOM);
+  packet_send(packet, client);
+  packet_destroy(packet);
+}
+
+void prompt_rooms() {
+  char input[64];
+
+  printf("Available rooms:\n");
+  for (u_int32_t i = 1; i <= rooms; i++) {
+    printf("%d.\n", i);
+  }
+  printf("Join: ");
+
+  read_line(input, sizeof(input));
+  u_int32_t room = strtoul(input, NULL, 10);
+  t_list *people_in_room = join_room(room);
+  int count = people_in_room->elements_count;
+
+  printf("--------------- ROOM %u ---------------\n", room);
+  if (count > 0)
+    printf("People in room: ");
+
+  for (u_int32_t i = 0; i < count; i++) {
+    const char *other = list_get(people_in_room, i);
+    printf("%s, ", other);
+  }
+
+  list_destroy(people_in_room);
+};
+
+void execute_command(const char *cmd) {
+  if (strcmp(cmd, "/quit") == 0) {
+    quit_program();
+  };
+
+  if (strcmp(cmd, "/leave") == 0) {
+    leave_room();
+    prompt_rooms();
+  }
 }
 
 void *handle_input(void *args) {
@@ -52,51 +166,45 @@ void *handle_input(void *args) {
 
   char message[500];
 
-  while (1) {
+  while (!quit) {
     read_line(message, sizeof(message));
-    send_message(message);
+    if (message[0] == '/') {
+      execute_command(message);
+    } else
+      send_message(message);
   }
+
   return NULL;
 };
 
-void login() {
-  if (client != -1)
-    connection_close(client);
-
-  client = connection_create_client("localhost", PORT);
-  printf("Enter name to login: ");
-
-  read_line(name, sizeof(name));
-  t_packet *packet = packet_create(LOGIN);
-  packet_add_string(packet, name);
-  packet_send(packet, client);
-  packet_destroy(packet);
-}
-
 int main(void) {
-  login();
+  bool logged_in = false;
+  while (!logged_in) {
+    logged_in = login();
+  }
+
+  t_packet *rooms_packet = packet_recieve(client);
+  rooms = get_rooms(rooms_packet);
+  prompt_rooms();
 
   pthread_t input;
   pthread_create(&input, NULL, &handle_input, NULL);
 
-  while (1) {
+  while (!quit) {
     t_packet *packet = packet_recieve(client);
+    if (!packet)
+      break;
+
     switch (packet->type) {
-
-    case USER_ALREADY_TAKEN: {
-      printf("Username already taken\n");
-      login();
-    } break;
-
-    case CONNECTED:
-      handle_connected(packet);
+    case JOINED:
+      handle_joined(packet);
       break;
 
-    case DISCONNECTED:
-      handle_disconnected(packet);
+    case LEFT:
+      handle_left(packet);
       break;
 
-    case MESSAGE_PACKET:
+    case MESSAGE:
       handle_message(packet);
       break;
     }
@@ -104,6 +212,5 @@ int main(void) {
   }
 
   pthread_join(input, NULL);
-  connection_close(client);
   return 0;
 }
--- chatroom/src/server.c
+++ chatroom/src/server.c
@@ -2,24 +2,35 @@
 #include <commons/log.h>
 #include <commons/net/connection.h>
 #include <commons/net/packet.h>
+#include <commons/net/status.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <time.h>
 
-#define PORT "8081"
+#define PORT "8080"
+#define AMOUNT_OF_ROOMS 3
 
 typedef enum {
   LOGIN,
-  CONNECTED,
-  DISCONNECTED,
   USER_ALREADY_TAKEN,
-  MESSAGE_PACKET,
+
+  JOINED,
+  LEFT,
+
+  ROOM_LIST,
+  JOIN_ROOM,
+  LEAVE_ROOM,
+
+  MESSAGE,
 } Packet_type;
 
 typedef struct {
   int socket;
   char *name;
   pthread_t thread;
+  u_int32_t room;
 } Client;
 
 t_log *logger;
@@ -27,23 +38,33 @@ int server;
 t_list *clients = NULL;
 
 int get_client_index(const char *name) {
-  t_list_iterator *iterator = list_iterator_create(clients);
-  while (list_iterator_has_next(iterator)) {
-    const Client *next = list_iterator_next(iterator);
-    if (strcmp(next->name, name) == 0) {
-      list_iterator_destroy(iterator);
-      return list_iterator_index(iterator);
-    }
+  for (int i = 0; i < clients->elements_count; i++) {
+    const Client *next = list_get(clients, i);
+    if (strcmp(next->name, name) == 0)
+      return i;
   }
-  list_iterator_destroy(iterator);
   return -1;
 }
 
-void broadcast(const char *from, t_packet *packet) {
+t_list *get_clients_in_room(u_int32_t room) {
+  t_list *res = list_create();
+  for (int i = 0; i < clients->elements_count; i++) {
+    Client *next = list_get(clients, i);
+    if (next->room == room)
+      list_add(res, next);
+  }
+  return res;
+}
+
+void broadcast(Client from, t_packet *packet) {
   t_list_iterator *iterator = list_iterator_create(clients);
   while (list_iterator_has_next(iterator)) {
     Client *next = list_iterator_next(iterator);
-    if (strcmp(next->name, from) == 0)
+
+    if (next->room != from.room)
+      continue;
+
+    if (strcmp(next->name, from.name) == 0)
       continue;
     packet_send(packet, next->socket);
   }
@@ -60,26 +81,56 @@ void handle_disconnected(Client client) {
   log_info(logger, "%s disconnected", client.name);
   unregister_client(client);
 
-  t_packet *packet = packet_create(DISCONNECTED);
+  t_packet *packet = packet_create(LEFT);
   packet_add_string(packet, client.name);
-  broadcast(client.name, packet);
+  broadcast(client, packet);
   packet_destroy(packet);
+}
+
+void handle_join(Client *client, u_int32_t room) {
+  log_info(logger, "%s joined room %u", client->name, client->room);
+
+  t_packet *res = status_pack(OK);
+
+  // TODO: get
+  t_list *clients_in_same_room = get_clients_in_room(room);
+  int count = clients_in_same_room->elements_count;
+  packet_add_uint32(res, count);
+  for (int i = 0; i < count; i++) {
+    const Client *next = list_get(clients, i);
+    packet_add_string(res, next->name);
+  }
+
+  list_destroy(clients_in_same_room);
+  packet_send(res, client->socket);
+  packet_destroy(res);
+
+  client->room = room;
+
+  t_packet *packet = packet_create(JOINED);
+  packet_add_string(packet, client->name);
+  broadcast(*client, packet);
+  packet_destroy(packet);
+}
+
+void handle_left(Client *client) {
+  log_info(logger, "%s left room %u", client->name, client->room);
+  t_packet *packet = packet_create(LEFT);
+  packet_add_string(packet, client->name);
+  broadcast(*client, packet);
+  packet_destroy(packet);
+
+  client->room = 0;
 }
 
 void handle_message(Client client, char *message) {
   log_info(logger, "Message recieved from %s", client.name);
 
-  t_packet *packet = packet_create(MESSAGE_PACKET);
+  t_packet *packet = packet_create(MESSAGE);
   packet_add_string(packet, client.name);
   packet_add_string(packet, message);
-  broadcast(client.name, packet);
+  broadcast(client, packet);
   packet_destroy(packet);
-}
-
-char *handle_login(t_packet *packet) {
-  char *name = packet_read_string(packet);
-  packet_destroy(packet);
-  return name;
 }
 
 void handle_user_taken(Client client) {
@@ -90,41 +141,69 @@ void handle_user_taken(Client client) {
   connection_close(client.socket);
 }
 
-void handle_connected(Client client) {
-  t_packet *packet = packet_create(CONNECTED);
-  packet_add_string(packet, client.name);
-  broadcast(client.name, packet);
+void send_rooms(Client client) {
+  t_packet *packet = packet_create(AMOUNT_OF_ROOMS);
+  packet_add_uint32(packet, AMOUNT_OF_ROOMS);
+  packet_send(packet, client.socket);
   packet_destroy(packet);
 }
 
-void *handle_client_connection(void *args) {
-  Client client = *(Client *)args;
-  t_packet *login_packet = packet_recieve(client.socket);
-
+void handle_login(Client *client) {
+  t_packet *login_packet = packet_recieve(client->socket);
   if (login_packet->type == LOGIN) {
-    client.name = handle_login(login_packet);
-    if (get_client_index(client.name) != -1) {
-      handle_user_taken(client);
-      return NULL;
-    };
-  } else {
-    unregister_client(client);
-    return NULL;
-  }
+    char *name = packet_read_string(login_packet);
 
-  list_add(clients, &client);
-  log_info(logger, "%s connected", client.name);
-  handle_connected(client);
+    if (get_client_index(name) != -1)
+      handle_user_taken(*client);
+
+    client->name = name;
+    list_add(clients, client);
+    log_info(logger, "%s connected", client->name);
+
+    t_packet *res = status_pack(OK);
+    packet_send(res, client->socket);
+    packet_destroy(res);
+
+    send_rooms(*client);
+  } else
+    unregister_client(*client);
+
+  packet_destroy(login_packet);
+}
+
+void *handle_client_connection(void *args) {
+  Client *client = (Client *)args;
+  handle_login(client);
 
   while (1) {
-    t_packet *packet = packet_recieve(client.socket);
+    t_packet *packet = packet_recieve(client->socket);
     if (!packet) {
-      handle_disconnected(client);
+      handle_disconnected(*client);
       return NULL;
     }
-    if (packet->type == MESSAGE_PACKET) {
+
+    switch (packet->type) {
+    case LEAVE_ROOM: {
+      handle_left(client);
+      break;
+    }
+
+    case JOIN_ROOM: {
+      u_int32_t room = packet_read_uint32(packet);
+      if (room > AMOUNT_OF_ROOMS) {
+        // TODO: probably send an error
+        handle_disconnected(*client);
+        return NULL;
+      }
+      handle_join(client, room);
+      break;
+    }
+
+    case MESSAGE: {
       char *message = packet_read_string(packet);
-      handle_message(client, message);
+      handle_message(*client, message);
+      break;
+    }
     }
   }
   return NULL;
